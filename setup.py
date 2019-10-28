1ec34b48540a9dc6952af0a71ff63cf5ff220191
bmulas1535 <bmulas1535@users.noreply.github.com>
2019-10-28 13:55:07 -0700

    configure setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+"""
+Setup Documentation
+"""
+import setuptools 
+
+REQUIRED = [
+    "numpy",
+    "pandas"
+    "matplotlib"
+    "sklearn"
+]
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+    setuptools.setup(
+    name="lambdata-bmulas1535_a",
+    version = "0.1.1",
+    author = "bmulas1535_a",
+    description = "A Collection of Data Science Helper Functions",
+    long_description = LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://lambdaschool.com/courses/data-science",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires = REQUIRED,
+    classifiers=["Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ]
+    )
